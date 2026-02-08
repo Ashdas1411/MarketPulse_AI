a3b5a94c852af3812955f412d3ade2b3c46d2502
Ashdas1411 <arkadas1411@gmail.com>
2026-02-08 14:13:39 +0530

    Update train.py

--- Backend/ML/train.py
+++ Backend/ML/train.py
@@ -5,7 +5,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report
 import joblib
 
-# 1. Load dataset
+# 1. Loading dataset
 data = pd.read_csv("ML/training_data.csv")
 
 # Features
@@ -14,24 +14,24 @@ X = data[["change_percent", "avg_sentiment"]]
 # Labels
 y = data["label"]
 
-# 2. Encode labels (BUY / HOLD / SELL -> numbers)
+# 2. Encoding labels (BUY / HOLD / SELL -> numbers)
 label_encoder = LabelEncoder()
 y_encoded = label_encoder.fit_transform(y)
 
-# 3. Split into training and testing data
+# 3. Spliting into training and testing data
 X_train, X_test, y_train, y_test = train_test_split(
     X, y_encoded, test_size=0.2, random_state=42
 )
 
-# 4. Train the model
+# 4. Training the model
 model = LogisticRegression(max_iter=1000)
 model.fit(X_train, y_train)
 
-# 5. Evaluate the model
+# 5. Evaluating the model
 y_pred = model.predict(X_test)
 print(classification_report(y_test, y_pred, target_names=label_encoder.classes_))
 
-# 6. Save model and encoder
+# 6. Saving model and encoder
 joblib.dump(model, "ML/trading_model.pkl")
 joblib.dump(label_encoder, "ML/label_encoder.pkl")
 
